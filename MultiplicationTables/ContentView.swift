7a6bd400abb20ddeca0c7787c68b6147be4185ba
Milo Wyner <milowyner@gmail.com>
2021-07-22 15:34:11 -0700

    Initial Commit

---
+++ MultiplicationTables/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MultiplicationTables
+//
+//  Created by Milo Wyner on 7/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
