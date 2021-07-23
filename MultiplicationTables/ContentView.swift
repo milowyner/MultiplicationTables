3bca4c44e65e20c31e85d30d2fbfd0db967d541b
Milo Wyner <milowyner@gmail.com>
2021-07-23 11:34:31 -0700

    Create basic starting screen with options to choose which multiplication table and how many questions to ask

--- MultiplicationTables/ContentView.swift
+++ MultiplicationTables/ContentView.swift
@@ -8,9 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var tableIndex = 5
+    @State var selectedNumberOfQuestions = 0
+    
+    let numberOfQuestions = ["5", "10", "20", "All"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Picker("Choose a multiplication table", selection: $tableIndex) {
+                    ForEach(1..<13) {
+                        Text("\($0)")
+                    }
+                }
+                Section {
+                    VStack(alignment: .leading) {
+                        Text("Number of questions:")
+                        Picker("Number of questions", selection: $selectedNumberOfQuestions) {
+                            ForEach(0..<numberOfQuestions.count) {
+                                Text(numberOfQuestions[$0])
+                            }
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                    }
+                }
+                
+                Section {
+                    Button("Start Game") {
+                        
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+            }
+            .navigationTitle("Multiplication Tables")
+        }
     }
 }
 
