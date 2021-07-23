4b63315fd719bcd720a07e1f0a2721f134f80205
Milo Wyner <milowyner@gmail.com>
2021-07-23 12:03:12 -0700

    Add second screen that fades in when "start game" is pressed (just a text field and button to end the game for now)

--- MultiplicationTables/ContentView.swift
+++ MultiplicationTables/ContentView.swift
@@ -10,37 +10,57 @@ import SwiftUI
 struct ContentView: View {
     @State var tableIndex = 5
     @State var selectedNumberOfQuestions = 0
+    @State var gameIsActive = false
     
     let numberOfQuestions = ["5", "10", "20", "All"]
     
     var body: some View {
-        NavigationView {
-            Form {
-                Picker("Choose a multiplication table", selection: $tableIndex) {
-                    ForEach(1..<13) {
-                        Text("\($0)")
-                    }
-                }
-                Section {
-                    VStack(alignment: .leading) {
-                        Text("Number of questions:")
-                        Picker("Number of questions", selection: $selectedNumberOfQuestions) {
-                            ForEach(0..<numberOfQuestions.count) {
-                                Text(numberOfQuestions[$0])
+        Group {
+            if gameIsActive {
+                NavigationView {
+                    VStack {
+                        Text("Hello game!")
+                        Button("End Game") {
+                            withAnimation {
+                                gameIsActive = false
                             }
                         }
-                        .pickerStyle(SegmentedPickerStyle())
                     }
+                    .navigationTitle("Multiplication Tables")
                 }
-                
-                Section {
-                    Button("Start Game") {
+            } else {
+                NavigationView {
+                    Form {
+                        Picker("Choose a multiplication table", selection: $tableIndex) {
+                            ForEach(1..<13) {
+                                Text("\($0)")
+                            }
+                        }
                         
+                        Section {
+                            VStack(alignment: .leading) {
+                                Text("Number of questions:")
+                                Picker("Number of questions", selection: $selectedNumberOfQuestions) {
+                                    ForEach(0..<numberOfQuestions.count) {
+                                        Text(numberOfQuestions[$0])
+                                    }
+                                }
+                                .pickerStyle(SegmentedPickerStyle())
+                            }
+                        }
+                        
+                        Section {
+                            Button("Start Game") {
+                                withAnimation {
+                                    gameIsActive = true
+                                }
+                            }
+                            .frame(maxWidth: .infinity)
+                        }
                     }
-                    .frame(maxWidth: .infinity)
+                    .navigationTitle("Multiplication Tables")
                 }
             }
-            .navigationTitle("Multiplication Tables")
         }
     }
 }
